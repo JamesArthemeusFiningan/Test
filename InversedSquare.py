b052729f24a26f45d73ced1d389d5ba60665b682
JamesArthemeusFiningan <oesch.jason@gmail.com>
2021-11-16 22:18:21 +0100

    f or timozhy

--- InversedSquare.py
+++ InversedSquare.py
@@ -7,21 +7,15 @@ from datetime import datetime
 def main():
     start = 1
     end = 100
-    reslst = []
-    stime = datetime.now()
+    stime=datetime.now()
     for start in range(end):
-        test = 1
-        for test in range(end):
-            if test == start:
-                continue
-            if test**start == start**test:
-                reslst.append(str(f'{start},{test}'))
-            test += 1
-        start += 1
-    etime = datetime.now()
-    print(reslst)
-    print(etime - stime)
-
+        i = 1
+        for i in range(end):
+            print(start**i)
+            i+=1
+        start+=1
+    etime=datetime.now()
+    print(etime-stime)
 
 if __name__ == "__main__":
     main()
